e1958e20932098320f5acdbcffc3acb8e77c5cc9
Necromoon <71983910+Necromoon@users.noreply.github.com>
2021-11-14 23:11:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import sys
+mport sys
 from os import path
 from sprites import *
 from tilemap import *
 
 
+# Game
 class Game:
     def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pg.display.set_mode((W, H))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.load_data()
@@ -15,14 +16,21 @@ class Game:
     def load_data(self):
         game_folder = path.dirname(__file__)
         self.map = Map(path.join(game_folder, 'map.txt'))
+        self.player_img = pg.image.load(path.join(game_folder, PLAYER_IMG)).convert_alpha()
+        self.enemy_img = pg.image.load(path.join(game_folder, ENEMY_IMG)).convert_alpha()
+        self.wall_img = pg.image.load(path.join(game_folder, WALL_IMG)).convert_alpha()
+        self.wall_img = pg.transform.scale(self.wall_img, (TS, TS))
 
     def new(self):
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
         for row, tiles in enumerate(self.map.data):
             for col, tile in enumerate(tiles):
                 if tile == '*':
                     Wall(self, col, row)
+                if tile == 'E':
+                    Enemy(self, col, row)
                 if tile == 'P':
                     self.player = Player(self, col, row)
         self.camera = Camera(self.map.width, self.map.height)
@@ -30,7 +38,7 @@ class Game:
     def run(self):
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
+            self.dt = self.clock.tick(FPS) / 1000.0
             self.events()
             self.update()
             self.draw()
@@ -44,14 +52,14 @@ class Game:
         self.camera.update(self.player)
 
     def draw_grid(self):
-        for x in range(0, WIDTH, TS):
-            pg.draw.line(self.screen, LG, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TS):
-            pg.draw.line(self.screen, LG, (0, y), (WIDTH, y))
+        for x in range(0, W, TS):
+            pg.draw.line(self.screen, LG, (x, 0), (x, H))
+        for y in range(0, H, TS):
+            pg.draw.line(self.screen, LG, (0, y), (W, y))
 
     def draw(self):
+        pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
         self.screen.fill(BGCOLOR)
-        self.draw_grid()
         for sprite in self.all_sprites:
             self.screen.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
@@ -77,3 +85,4 @@ while True:
     g.new()
     g.run()
     g.show_go_screen()
+
